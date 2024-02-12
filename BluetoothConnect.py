da6d50c8e1ffff70b159a756bbeeac5d856bb0ab
Amine <elarifamine1gmail.com>
2024-02-12 11:21:27 +0100

    Final version

--- BluetoothConnect.py
+++ BluetoothConnect.py
@@ -1,6 +1,7 @@
 import asyncio
 from bleak import BleakClient, BleakScanner
-
+import time
+import struct
 async def device(mac):
     # scan for devices
     devices = await BleakScanner.discover()
@@ -9,15 +10,20 @@ async def device(mac):
             return d
     return None
 
+byte_array = bytearray(b'T10\x00\x00\x00\x00\x00_\x08\x00\x00n\xe1\x0b\xfeH\x08er\xdc\x00\x00\x00')
+hex_string = ''.join('{:02x}'.format(byte) for byte in byte_array)
+print(hex_string)
+
 def getNotified(sender,data):
-    print('here is your data sir')
+    
     print(sender, data)
 
 async def main():
-    d = await device('D1:6A:16:01:A2:EC')
+    d = await device('DC:DC:FB:72:D4:56')
     async with BleakClient(d.address) as client:
         if client.is_connected:
             ref = {}
+            numValue = 0
             print('connected')
             services = await client.get_services()
             for service in services:
@@ -25,36 +31,31 @@ async def main():
                     temp = str(str(charac) + " UNKNOWN").split()
                     ref[temp[0] + '-' + temp[3]] = temp[1:][1][:-2]
             
-            print(ref)
-            
-            # Write to the LIST characteristic first
             # for key in ref:
-            #     if key.split('-')[-1] == 'LIST':
-       
-            #         await client.write_gatt_char(int(ref[key]), bytearray(b'\x17\x00'))
+            #     try:
+            #          await client.start_notify(int(ref[key]), getNotified)
+            #          print(key.split('-')[-1] + 'subbed')
+            #     except:
+                    
+            #         pass
+                    
             for key in ref:
                 if key.split('-')[-1] == 'LIST':
-                    await client.write_gatt_char(int(ref[key]), bytearray(b'\x17\x00'))
-            for key in ref:
-                if key.split('-')[-1] == 'LOG':
-       
-                    await  client.write_gatt_char(int(ref[key]), bytearray(b'\x02\x13\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'))
-            # Then start notifications
-            for key in ref:
-                if key.split('-')[-1] == 'ACK':
-                   
                     await client.start_notify(int(ref[key]), getNotified)
-            
-            
 
+            
+            # Write to the LIST characteristic first
+            
             for key in ref:
                 if key.split('-')[-1] == 'NUM':
-                    await client.read_gatt_char(int(ref[key]))
+                    numValue = await client.read_gatt_char(int(ref[key]))
             for key in ref:
-                if key.split('-')[-1] == 'LOG':
-              
-                    print(await client.read_gatt_char(int(ref[key])))
+                if key.split('-')[-1] == 'LIST':
+                    
+                    await client.write_gatt_char(int(ref[key]), numValue)
+            time.sleep(2)
             
+           
             
 asyncio.run(main())
 
